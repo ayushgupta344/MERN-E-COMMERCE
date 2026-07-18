b17ad87cfe9c7fb72965956102f11f2ae6a17c89
ayushgupta344 <ayushgupta5234@gmail.com>
2026-07-19 00:07:25 +0530

    Initial commit

---
+++ MERN E-commerce/backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors =  require('cors')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
+dotenv.config()
+connectDB()
+
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.use('/api/auth',userRoutes)
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
