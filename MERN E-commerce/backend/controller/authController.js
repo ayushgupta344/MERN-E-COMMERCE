3fdc3e54d2f9f92fceb02ae727aea1932620c963
ayushgupta344 <ayushgupta5234@gmail.com>
2026-07-23 01:56:33 +0530

    Auth routes created

--- MERN E-commerce/backend/controller/authController.js
+++ MERN E-commerce/backend/controller/authController.js
@@ -1,4 +1,10 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { sendEmail } = require("../utils/sendEmail");
+const generateToken = (id)=>{
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "30d"})   
+}
 const registerUser = async (req, res) => {
     const { name, email, password } = req.body; 
     try {
@@ -6,10 +12,56 @@ const registerUser = async (req, res) => {
         if(existingUser){
             return res.status(400).json({ message: "User already exists"})
         }
-        const newUser = new User({ name, email, password})
-        await newUser.save()
-        res.status(201).json({ message: "User registered successfully"})    
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+        const user = await User.create({ name, email, password: hashedPassword})
+        if(user){
+            const otp= Math.floor(100000 + Math.random() * 900000)
+            const message=
+            `Welcome to ShopNest,${name}! Thank you for registering.
+             Your OTP for ShopNest is ${otp}` 
+            await sendEmail(email, "Welcome to ShopNest - Your OTP for registration!", message)
+        }
+        res.status(201).json(
+            {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                token: generateToken(user._id),
+                message: "User registered successfully. Please check your email for the OTP."
+            }
+        )
     } catch (error) {
         res.status(500).json({ message: "Internal server error" })
     }
 }
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if(user && (await bcrypt.compare(password, user.password))){
+            res.status(200).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,    
+                token: generateToken(user._id),
+            })
+        } else {
+            res.status(401).json({ message: "Invalid email or password" })
+        }  
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" })
+    } 
+} 
+const getUsers = async (req, res) => {
+    try{
+        const users = await User.find({}).select("-password");
+        res.status(200).json(users);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Internal server error" })
+    }  
+}
+module.exports = { registerUser, loginUser, getUsers };  
--- MERN E-commerce/backend/index.js
+++ MERN E-commerce/backend/index.js
@@ -7,10 +7,14 @@ dotenv.config()
 connectDB()
 
 const app = express()
+app.use(cors())
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.use('/api/auth',userRoutes)
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
