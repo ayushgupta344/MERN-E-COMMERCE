02ffc612343e958f25391697f8139b565a58d5be
ayushgupta344 <ayushgupta5234@gmail.com>
2026-07-30 20:20:56 +0530

    Debug express issues

--- backend/index.js
+++ backend/index.js
@@ -63,7 +63,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // React Router
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 } else {
